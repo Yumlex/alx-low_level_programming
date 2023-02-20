305ec9f4486882952c24df8c48611a258acd335b
yumna <yumnaphilander@gmail.com>
2023-02-20 05:51:10 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alpabet without q and e
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i 1= 101 && i 1= 113)
+		{	
+		putchar(i);
+		}
+        }
+        putchar('\n');
+        return (0);
