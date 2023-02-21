0e39236b28677a0d06325cff0ae26c2848b87da5
yumna <yumnaphilander@gmail.com>
2023-02-21 11:56:04 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet_x10- make alphabet x10 times
+ * Return void
  */
 
 void print_alphabet_x10(void)
 {
-		char letter;
+		char c;
+		int i = 0;
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
+		while (i <= 0)
+		{
+		for (c = 'a'; c  <= 'z'; c++)
+		{
+		_putchar(c);
+		}
 		_putchar('\n');
+		i++;
+		}
 }
